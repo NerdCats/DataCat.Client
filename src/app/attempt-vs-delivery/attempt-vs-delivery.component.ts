d942f4ab8cce1a4153d2eb1f3564d95c44201e8b
Asadullah Rifat <anonymusrif@gmail.com>
2017-03-27 14:47:26 +0600

    GFETCH-398 new kebab-case

---
+++ src/app/attempt-vs-delivery/attempt-vs-delivery.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardEventService } from '../dashboard/dashboard-event.service';
+import { DataService } from '../data/index';
+import { LoggerService } from '../shared/index';
+
+@Component({
+    moduleId: module.id,
+    selector: 'as-attempt-vs-delivery',
+    templateUrl: 'attempt-vs-delivery.html'
+})
+export class AttemptVsDeliveryComponent implements OnInit {
+    public isDataAvailable: boolean = false;
+    public AttemptVsDeliveryData: any[];
+    constructor(
+        private dataService: DataService,
+        private loggerService: LoggerService,
+        private dashboarEventService: DashboardEventService) {
+        dashboarEventService.componentUpdated({ Event: 'loaded', Name: 'Attempt VS Delivery' });
+    }
+    ngOnInit() {
+        let document: any = {
+            'aggregate':
+            [
+                {
+                    $project: {
+                        _id: 1, HRID: 1, Name: 1, State: 1, CreateTime: 1, ModifiedTime: 1, CompletionTime: 1, AttemptCount: 1,
+                        Tasks: { $slice: ['$Tasks', -1] }
+                    }
+                },
+                {
+                    $match: {
+                        'AttemptCount': { $gte: 1 },
+                        'State': 'COMPLETED',
+
+                        'Tasks.IsTerminatingTask': true,
+                        'Tasks.State': 'COMPLETED',
+                        'Tasks.Type': 'Delivery',
+                    }
+                },
+                {
+                    $group: {
+                        _id: '$AttemptCount',
+                        DeliveredOrders: { $sum: 1 },
+                        AvgTimeToDelivery: { $avg: { $divide: [{ $subtract: ['$CompletionTime', '$CreateTime'] }, 86400000] } }
+                    }
+                },
+                { $sort: { _id: 1 } }
+            ]
+        };
+        this.dataService.executeAggregation('Jobs', document)
+            .subscribe(result => {
+                let jobCountArray: any[] = [];
+                jobCountArray = result;
+                this.AttemptVsDeliveryData = [{ data: jobCountArray, label: 'Attempt VS Delivery' }];
+                this.isDataAvailable = true;
+            },
+            error => { this.loggerService.error(error); });
+    }
+}
---
+++ src/app/attempt-vs-delivery/index.ts
@@ -0,0 +1,1 @@
+export * from './attempt-vs-delivery.component';
