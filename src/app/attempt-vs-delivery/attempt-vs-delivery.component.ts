35b6528d5b0728ae753fd417d83d251719065072
Asadullah Rifat <anonymusrif@gmail.com>
2017-04-04 12:36:12 +0600

    GFETCH-410 Commit Before starting work

--- src/app/attempt-vs-delivery/attempt-vs-delivery.component.ts
+++ src/app/attempt-vs-delivery/attempt-vs-delivery.component.ts
@@ -156,8 +156,6 @@ export class AttemptVsDeliveryComponent implements OnInit {
                     }
                 },
                 error => { this.loggerService.error(error); });
-
-
         }
 
     }
