d3732f9e5b149590e3f6b54795ff32e0945e1499
Swagata Prateek <thehoneymad@hotmail.com>
2017-05-01 15:04:49 +0200

    Adding a generic feed class to support paginated feeds

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -2,3 +2,4 @@ export * from './data.service';
 export * from './data.module';
 export * from './dashboard/dashboard-config';
 export * from './dashboard/dashboard-widget';
+export * from './page-envelope';
---
+++ src/app/data/page-envelope.ts
@@ -0,0 +1,8 @@
+export interface PageEnvelope {
+    total: number;
+    page: number;
+    start: number;
+    pageSize: number;
+    totalPages: number;
+    returned: number;
+}
