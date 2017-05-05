d1523ca33b40c8a75db2a01d5c9fc960ffe97cb7
Swagata Prateek <thehoneymad@hotmail.com>
2017-05-05 02:41:18 +0200

    Making navbar have proper router links

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './index';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { LetterAvatarModule } from '../shared/letter-avatar/index';
 
 @NgModule({
@@ -12,6 +14,7 @@ import { LetterAvatarModule } from '../shared/letter-avatar/index';
     ],
     imports: [
         CommonModule,
+        RouterModule,
         LetterAvatarModule
     ]
 })
